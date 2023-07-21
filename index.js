92d9e1cb15b6b268b5a357c674f844127b47e1f6
sowmik-sec <sowmik.sec@gmail.com>
2023-07-21 15:08:20 +0600

    create class and constructor

---
+++ index.js
@@ -0,0 +1,17 @@
+const person = {};
+person.name = "Sowmik";
+person.email = "sowmik.sec@gmail.com";
+console.log(person);
+
+class Person {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+}
+const p1 = new Person("Ahsan", "ahsan@gmail.com");
+const p2 = new Person("Habib", "habib@gmail.com");
+const p3 = new Person("Sowmik", "sowmik.sec@gmail.com");
+console.log(p1);
+console.log(p2);
+console.log(p3);
