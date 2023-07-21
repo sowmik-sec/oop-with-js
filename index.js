52387474735c68c4d6fe6a9412e6d52ec07552ae
sowmik-sec <sowmik.sec@gmail.com>
2023-07-21 15:29:06 +0600

    private member

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ console.log(person);
 
 class Person {
   constructor(name, email) {
-    this.name = name;
-    this.email = email;
+    this._name = name;
+    this._email = email;
+  }
+  getName() {
+    return this._name;
+  }
+  getEmail() {
+    return this._email;
+  }
+  setName(name) {
+    this._name = name;
+  }
+  setEmail(email) {
+    this._email = email;
   }
   changeName(name) {
     this.name = name;
   }
   sendEmail(msg) {
+    console.log(this.email);
     console.log("Sending Email", msg);
+  }
+  sendMail(msg) {
+    console.log("To", this._email);
+    console.log("MSG:", this._sanitizeMsg(msg));
+  }
+  _sanitizeMsg(msg) {
+    return msg.trim().toLowerCase();
+  }
+  print() {
+    console.log(this);
   }
 }
 const p1 = new Person("Ahsan", "ahsan@gmail.com");
 const p2 = new Person("Habib", "habib@gmail.com");
 const p3 = new Person("Sowmik", "sowmik.sec@gmail.com");
+
+console.log(p1.getName());
+
+p1.setName("fadsfasd");
 console.log(p1);
-console.log(p2);
-console.log(p3);
+
+p1.sendMail("        WOOW! IT IS WORKING");
