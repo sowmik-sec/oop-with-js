6f50c075cd8734adb4dca3bed5c4c5808a582ad3
sowmik-sec <sowmik.sec@gmail.com>
2023-07-21 15:51:00 +0600

    toString override

---
+++ toStringOverride.js
@@ -0,0 +1,30 @@
+const person = {};
+person.name = "Sowmik";
+person.email = "sowmik.sec@gmail.com";
+console.log(person);
+
+class Person {
+  constructor(name, email) {
+    this._name = name;
+    this._email = email;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (name === "Ahsan") return;
+    this._name = name;
+  }
+  print() {
+    console.log(this);
+  }
+  toString() {
+    return `Name: ${this._name}, Email: ${this._email}`;
+  }
+}
+const p1 = new Person("Ahsan", "ahsan@gmail.com");
+const p2 = new Person("Habib", "habib@gmail.com");
+const p3 = new Person("Sowmik", "sowmik.sec@gmail.com");
+
+p1.print();
+console.log(p1 + "");
