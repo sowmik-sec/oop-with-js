c55eef73b752376e80e8be8b488d9655a62a3ddf
sowmik-sec <sowmik.sec@gmail.com>
2023-07-22 11:54:25 +0600

    create department class

--- university-management-system/contact/Contact.js
+++ university-management-system/contact/Contact.js
@@ -6,10 +6,10 @@ const _address = Symbol("address");
 class Contact {
   constructor({ id, email, phone, alternativePhone, address }) {
     this[_id] = id;
-    this[_email] = email;
-    this[_phone] = phone;
-    this[_alternativePhone] = alternativePhone;
-    this[_address] = address;
+    this[_email] = email || "";
+    this[_phone] = phone || "";
+    this[_alternativePhone] = alternativePhone || "";
+    this[_address] = address || "";
   }
   get id() {
     return this[_id];
